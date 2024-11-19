4717456cb55bb1b471a349768e816d656dbd3013
Tiago Gomes <tidgomes@gmail.com>
2024-11-19 10:53:44 -0300

    all about gets

--- src/main/java/com/tidz/sbur_rest_demo/SburRestDemoApplication.java
+++ src/main/java/com/tidz/sbur_rest_demo/SburRestDemoApplication.java
@@ -2,7 +2,14 @@ package com.tidz.sbur_rest_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -36,6 +43,38 @@ public class SburRestDemoApplication {
         public void setName(String name) {
             this.name = name;
         }
+    }
+
+    @RestController
+    @RequestMapping("/")
+    class RestApiDemoController {
+        private List<Coffee> coffees = new ArrayList<>();
+
+        public RestApiDemoController() {
+            coffees.addAll(List.of(
+                    new Coffee("Café Cereza"),
+                    new Coffee("Café Ganador"),
+                    new Coffee("Café Lareño"),
+                    new Coffee("Café Três Pontas")
+            ));
+        }
+
+        @GetMapping("/coffees")
+        Iterable<Coffee> getCoffees() {
+            return coffees;
+        }
+
+        @GetMapping("/coffees/{id}")
+        Optional<Coffee> getCoffeeById(@PathVariable String id) {
+            for (Coffee c : coffees) {
+                if (c.getId().equals(id)) {
+                    return Optional.of(c);
+                }
+            }
+
+            return Optional.empty();
+        }
+
     }
 
 }
